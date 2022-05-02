76f19f50fe90315dc1136cd85122b868b31145a5
s1n7 <luisbruemmer@gmail.com>
2022-05-02 19:09:34 +0200

    Create computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello Game!")
+function computerPlay() {
+    let rand = Math.floor(Math.random() * 3);   //create random integer between 0 and 2
+    const values = ["Rock", "Paper", "Scissors"]    //array with values
+    return values[rand];    //return randomly chosen value from array
+}
+
