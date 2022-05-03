2a88f724ba5103edeabaf88668785b689d0cffc1
s1n7 <luisbruemmer@gmail.com>
2022-05-03 11:42:55 +0200

    Make computerPlay array values lowercase

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function computerPlay() {
     let rand = Math.floor(Math.random() * 3);   //create random integer between 0 and 2
-    const values = ["Rock", "Paper", "Scissors"]    //array with values
+    const values = ["rock", "paper", "scissors"]    //array with values
     return values[rand];    //return randomly chosen value from array
 }
 
