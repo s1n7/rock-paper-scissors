3091fc406c16fe7185a410940e03ddb87c33e68e
s1n7 <luisbruemmer@gmail.com>
2022-05-24 17:33:14 +0200

    Add comments, beautify code structure

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-function computerPlay() {   // function to create random value
+//create random value computer choice
+function computerPlay() {   
     let rand = Math.floor(Math.random() * 3);   //create random integer between 0 and 2
     const values = ["rock", "paper", "scissors"];   
-    return values[rand];    //return randomly chosen value from array
+    return values[rand];
 }
 
-function play(playerSelection, computerSelection) {
-    while (["rock", "paper", "scissors"].includes(playerSelection)) {   //check if user input is valid
-        if (playerSelection === computerSelection) {
-            return 0;
-        } else if (playerSelection === "rock" && computerSelection === "scissors") {
-            return 1;
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            return 1;
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            return 1;
-        } else if (computerSelection === "rock" && playerSelection === "scissors") {
-            return 2;
-        } else if (computerSelection === "paper" && playerSelection === "rock") {
-            return 2;
-        } else{
-            return 2;
-        }
-    }   
-    return;
+
+//play one round of rps
+function playRound(playerSelection, computerSelection) {    
+    if (playerSelection === computerSelection) {
+        return 0;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return 1;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return 1;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return 1;
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return 2;
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return 2;
+    } else{
+        return 2;
+    }
 }
 
+
+//select html elements
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.result');
 const roundResult = document.querySelector('.roundResult');
@@ -33,17 +35,22 @@ const reloadButton = document.createElement('button');
 reloadButton.setAttribute('onClick', 'window.location.reload();');
 reloadButton.textContent = 'Replay';
 
+
+//set initial values for player score, computer score, draws and rounds played to 0
 let playerScore = 0;
 let computerScore = 0;
 let draws = 0;
 let rounds = 0;
 
 
+//add event listener to buttons
 buttons.forEach(button => { button.addEventListener('click', playGame) });
 
+
+//play 5 rounds of rps
 function playGame(e) {
-    let playerSelection = e.target.id;
-    const point = play(playerSelection, computerPlay());
+    const playerSelection = e.target.id;
+    const point = playRound(playerSelection, computerPlay());
     rounds++;
     if (point === 0) {
         draws++;
@@ -58,12 +65,12 @@ function playGame(e) {
         document.getElementById('computerScore').textContent = computerScore;
         roundResult.textContent = 'The computer wins this round!';
     }
-    
     determineWinner();
-
     replay();
 }
 
+
+//determine winner after 5 rounds
 function determineWinner() {
     if (rounds === 5) {
         if (playerScore > computerScore) {  
@@ -77,6 +84,8 @@ function determineWinner() {
     }
 }
 
+
+//add replay button
 function replay() {
     reloadDiv.appendChild(reloadButton);
 }
